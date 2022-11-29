8b1a320c9b67dbf376b9192285db7cba45062295
mingliu <ming-liu@outlook.com>
2022-11-29 09:43:23 -0600

    product add one product

--- react-app/src/components/AddProductPage/index.js
+++ react-app/src/components/AddProductPage/index.js
@@ -5,37 +5,33 @@ import { addProductThunk } from '../../store/product';
 import './AddProductPage.css'
 
 function AddProductPage() {
-    const dispatch = useDispatch();
-    const { productId } = useParams();
-    const product = useSelector(state => state.products.singleProduct)
+    // const dispatch = useDispatch();
+    // const { productId } = useParams();
+    // const product = useSelector(state => state.products.singleProduct)
     // console.log("OneProductsPage product: ", product)
-    const [isLoaded, setIsLoaded] = useState(false);
+    // const [isLoaded, setIsLoaded] = useState(false);
 
-    useEffect(() => {
-        dispatch(addProductThunk(productId)).then(() => setIsLoaded(true));
-    }, [dispatch, productId]);
+    // useEffect(() => {
+    //     dispatch(addProductThunk(productId)).then(() => setIsLoaded(true));
+    // }, [dispatch, productId]);
 
-    if (!product) return null;
+    // if (!product) return null;
 
     return (
         <div>
             <h1>single product</h1>
-            {isLoaded && (
+            {/* {isLoaded && (
                 <div >
                     <div key={product.id}>
 
                         <div>
-                            {/* <img className='home-product-img' src={product.images[0].url} alt='Preview image' /> */}
                             {product.images.length && (
                                 product.images.map(image => (
                                     <img key={image.id} src={image.url} />
-                                    // <img key={image.id} src={image.url} onError={e => e.target.src = 'https://i.imgur.com/rIUtyi2.jpg'}/>
+
                                 ))
                             )}
                         </div>
-                        {/* <div>
-                            <img className='home-product-img' src={product.images[0].url} alt='Preview image' />
-                        </div> */}
                         <div>{product.name}</div>
                         <div>!!! review stars</div>
                         <div>in {product.category}</div>
@@ -47,12 +43,12 @@ function AddProductPage() {
                         <div>{product.dimension}</div>
                         <div>{product.brand}</div>
                         <div>{product.weight}</div>
-                        {/* <div>{product.quantity}</div> */}
+
                         <div> !!! reviews</div>
                         <div> !!! add to cart</div>
                     </div>
                 </div>
-            )}
+            )} */}
 
         </div>
     )
