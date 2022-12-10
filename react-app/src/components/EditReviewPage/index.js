582591e76f45948b533934d4fd9fb45b7db4fb43
mingliu <ming-liu@outlook.com>
2022-12-09 23:44:18 -0600

    add modal

---
+++ react-app/src/components/EditReviewPage/index.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { Modal } from '../../context/Modal';
+import { useSelector } from 'react-redux';
+import LoginForm from '../LoginFormModal/LoginForm.js';
+import AddNewReviewForm from './AddNewReviewForm';
