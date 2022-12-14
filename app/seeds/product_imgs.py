6f544cdef791c615d29afcf82210b3c685174e01
mingliu <ming-liu@outlook.com>
2022-12-13 23:01:42 -0600

    update aws seed

--- app/seeds/product_imgs.py
+++ app/seeds/product_imgs.py
@@ -7,19 +7,19 @@ def seed_product_imgs():
     # img id 1
     product001_img001 = Product_Img(
         product_id=1,
-        url='https://m.media-amazon.com/images/I/61s30lrLDSL._AC_SY300_SX300_.jpg',
+        url='https://mingprojectawsbucket.s3.amazonaws.com/seed/product_imgs/1_mzone_device/1_echodot5/1.jpg',
         # preview=True,
     )
     # img id 2
     product001_img002 = Product_Img(
         product_id=1,
-        url='https://m.media-amazon.com/images/I/71XqKlSdUCL._AC_SX679_.jpg',
+        url='https://mingprojectawsbucket.s3.amazonaws.com/seed/product_imgs/1_mzone_device/1_echodot5/2.jpg',
         # preview=False,
     )
     # img id 3
     product001_img003 = Product_Img(
         product_id=1,
-        url='https://m.media-amazon.com/images/I/61hvnjBzxlL._AC_SX679_.jpg',
+        url='https://mingprojectawsbucket.s3.amazonaws.com/seed/product_imgs/1_mzone_device/1_echodot5/3.jpg',
         # preview=False,
     )
 
@@ -27,19 +27,19 @@ def seed_product_imgs():
     # img id 4
     product002_img001 = Product_Img(
         product_id=2,
-        url='https://m.media-amazon.com/images/I/71MBZDz967L._AC_SY300_SX300_.jpg',
+        url='https://mingprojectawsbucket.s3.amazonaws.com/seed/product_imgs/1_mzone_device/2_echoshow15/1.jpg',
         # preview=True,
     )
     # img id 5
     product002_img002 = Product_Img(
         product_id=2,
-        url='https://m.media-amazon.com/images/I/813UdoM4Y2L._AC_SX679_.jpg',
+        url='https://mingprojectawsbucket.s3.amazonaws.com/seed/product_imgs/1_mzone_device/2_echoshow15/2.jpg',
         # preview=False,
     )
     # img id 6
     product002_img003 = Product_Img(
         product_id=2,
-        url='https://m.media-amazon.com/images/I/71FPX1G52qL._AC_SX679_.jpg',
+        url='https://mingprojectawsbucket.s3.amazonaws.com/seed/product_imgs/1_mzone_device/2_echoshow15/3.jpg',
         # preview=False,
     )
 
@@ -48,19 +48,19 @@ def seed_product_imgs():
     # img id 7
     product003_img001 = Product_Img(
         product_id=3,
-        url='https://m.media-amazon.com/images/I/91RNXNIf2gS.__AC_SX300_SY300_QL70_FMwebp_.jpg',
+        url='https://mingprojectawsbucket.s3.amazonaws.com/seed/product_imgs/2_mzone_home/3_coffetable/1.jpg',
         # preview=True,
     )
     # img id 8
     product003_img002 = Product_Img(
         product_id=3,
-        url='https://m.media-amazon.com/images/I/91cdU0eSSIS._AC_SX679_.jpg',
+        url='https://mingprojectawsbucket.s3.amazonaws.com/seed/product_imgs/2_mzone_home/3_coffetable/2.jpg',
         # preview=False,
     )
     # img id 9
     product003_img003 = Product_Img(
         product_id=3,
-        url='https://m.media-amazon.com/images/I/51m-ogeM6ZL._AC_SX679_.jpg',
+        url='https://mingprojectawsbucket.s3.amazonaws.com/seed/product_imgs/2_mzone_home/3_coffetable/3.jpg',
         # preview=False,
     )
 
@@ -68,19 +68,19 @@ def seed_product_imgs():
     # img id 10
     product004_img001 = Product_Img(
         product_id=4,
-        url='https://m.media-amazon.com/images/I/81nmQ0KRsgL._AC_SX679_.jpg',
+        url='https://mingprojectawsbucket.s3.amazonaws.com/seed/product_imgs/2_mzone_home/4_tvstand/1.jpg',
         # preview=True,
     )
     # img id 11
     product004_img002 = Product_Img(
         product_id=4,
-        url='https://m.media-amazon.com/images/I/91rGIa-dVfL._AC_SX679_.jpg',
+        url='https://mingprojectawsbucket.s3.amazonaws.com/seed/product_imgs/2_mzone_home/4_tvstand/2.jpg',
         # preview=False,
     )
     # img id 12
     product004_img003 = Product_Img(
         product_id=4,
-        url='https://m.media-amazon.com/images/I/71kWY0ER4DL._AC_SX679_.jpg',
+        url='https://mingprojectawsbucket.s3.amazonaws.com/seed/product_imgs/2_mzone_home/4_tvstand/3.jpg',
         # preview=False,
     )
 
