e13efa870d736b838a7cff2b338a9bf74404c056
mingliu <ming-liu@outlook.com>
2022-12-18 00:12:49 -0600

    nav bar account list

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,10 +1,81 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Modal } from '../../context/Modal';
-// import ProfileButton from './ProfileButton';
+import AccountList from './AccountList';
 // import LoginFormModal from '../LoginFormModal';
 // import SignupFormModal from '../SignupFormModal';
 // import AddNewSpotModal from '../AddNewSpotModal/index.js';
 // import AddNewSpotForm from '../AddNewSpotModal/AddNewSpotForm';
+import mzonelogo from '../../img/mzonelogo.png'
+import flag from '../../img/flag.png'
 import './Navigation.css';
+
+function Navigation() {
+    const user = useSelector(state => state.session.user);
+
+    // let sessionLinks;
+    // if (sessionUser) {
+    //     sessionLinks = (
+    //         <AccountButton user={sessionUser} />
+    //     );
+    // } else {
+    //     sessionLinks = (
+    //         <div >
+    //             <div >
+    //                 {/* <LoginFormModal /> */}
+    //             </div>
+    //             <div >
+    //                 {/* <SignupFormModal /> */}
+    //             </div>
+    //             {/* <NavLink to="/signup">Sign Up</NavLink> */}
+    //         </div>
+    //     );
+    // }
+
+    return (
+        <div className='nav-bar-container'>
+            <nav className='nav-bar-1'>
+                {/* left logo section */}
+                <div className='nav-bar1-logo'>
+                    <NavLink exact to='/'>
+                        <img src={mzonelogo} className='nav-bar1-logo-img' />
+                    </NavLink>
+                </div>
+                <hr></hr>
+                <div>
+                    <div>Developer: Ming Liu</div>
+                    <div>links</div>
+                </div>
+                <hr></hr>
+                {/* <div>select your address section</div> */}
+                {/* <div>search section</div> */}
+                <div>
+                    <img src={flag} className='nav-bar1-logo-imgflag' />
+                    <div>EN</div>
+                </div>
+                <hr></hr>
+                <div>
+                    <AccountList />
+                </div>
+                <hr></hr>
+                <div>
+                    <div>orders section</div>
+                </div>
+                <hr></hr>
+                <div>
+                    <div>item number</div>
+                    <div>cart logo </div>
+                    <div>Cart </div>
+                </div>
+            </nav>
+            <nav className='nav-bar-2'>
+                <div>second row different category</div>
+            </nav>
+        </div>
+    )
+
+
+}
+
+export default Navigation;
