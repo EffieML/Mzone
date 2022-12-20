2f3faa81531699efcb910d038991fb15e4779e02
mingliu <ming-liu@outlook.com>
2022-12-20 09:38:55 -0600

    nav bar account drop down

--- react-app/src/components/Navigation/AccountList.js
+++ react-app/src/components/Navigation/AccountList.js
@@ -13,31 +13,73 @@ function AccountList() {
     if (user) {
         sessionLinks = (
             <div className='nav-bar1-account-dropdown'>
-                <div><NavLink to={`/users/${user.id}`}>My Account</NavLink></div>
-                <div><NavLink to={`/orders`}>My Orders</NavLink></div>
-                <div><NavLink to={`/products/current`}>My Products</NavLink></div>
-                {/* <div><NavLink to={`/favorites`}>My Favorites</NavLink></div> */}
-                {/* <div><NavLink to={`/reviews`}>My Reviews</NavLink></div> */}
-                <div><LogoutButton /></div>
+                <div className='nav-bar1-account-dropdown-topu'>
+                    <div >
+                        {user.username} is shopping.
+                    </div>
+                </div>
+                <div className='nav-bar1-account-dropdown-bttm'>
+                    <div className='nav-bar1-account-dropdown-b1'>
+                        <div className='nav-bar1-account-b1-title'>Your Selling Account</div>
+                        <div className='nav-bar1-account-b1-word'><NavLink to={`/products/current`}>Products</NavLink></div>
+                    </div>
+                    <div className='nav-bar1-account-dropdown-b2'>
+                        <div className='nav-bar1-account-b1-title'>Your Account</div>
+                        <div className='nav-bar1-account-b1-word'><NavLink to={`/users/${user.id}`}>Account</NavLink></div>
+                        <div className='nav-bar1-account-b1-word'><NavLink to={`/orders`}>Orders</NavLink></div>
+                        {/* <div><NavLink to={`/favorites`}>My Favorites</NavLink></div> */}
+                        {/* <div><NavLink to={`/reviews`}>My Reviews</NavLink></div> */}
+                        <div className='nav-bar1-account-b1-word'><LogoutButton /></div>
+                    </div>
+                </div>
             </div>
         );
     } else {
         sessionLinks = (
             <div className='nav-bar1-account-dropdown'>
-                <div>
-                    <div><NavLink to={`/login`}><button>Sign In</button></NavLink></div>
-                    <div>
-                        New customer?
+                <div className='nav-bar1-account-dropdown-top'>
+                    <div ><NavLink to={`/login`}><button className='nav-bar1-account-dropdown-top-button'>Sign In</button></NavLink></div>
+                    <div className='nav-bar1-account-dropdown-top-word'>
+                        <div className='nav-bar1-account-dropdown-top-word1'>New customer?</div>
                         <NavLink to={`/sign-up`}> Start here.</NavLink>
                     </div>
                 </div>
-                <hr></hr>
-                <div><NavLink to={`/login`}>My Account</NavLink></div>
-                <div><NavLink to={`/login`}>My Orders</NavLink></div>
-                <div><NavLink to={`/login`}>My Products</NavLink></div>
-                {/* <div><NavLink to={`/login`}>My Favorites</NavLink></div> */}
-                {/* <div><NavLink to={`/login`}>My Reviews</NavLink></div> */}
+                <div className='nav-bar1-account-dropdown-bttm'>
+                    <div className='nav-bar1-account-dropdown-b1'>
+                        <div className='nav-bar1-account-b1-title'>Your Selling Account</div>
+                        <div className='nav-bar1-account-b1-word'><NavLink to={`/login`}>Products</NavLink></div>
+                    </div>
+                    <div className='nav-bar1-account-dropdown-b2'>
+                        <div className='nav-bar1-account-b1-title'>Your Account</div>
+                        <div className='nav-bar1-account-b1-word'><NavLink to={`/login`}>Account</NavLink></div>
+                        <div className='nav-bar1-account-b1-word'><NavLink to={`/login`}>Orders</NavLink></div>
+                        {/* <div><NavLink to={`/login`}>My Favorites</NavLink></div> */}
+                        {/* <div><NavLink to={`/login`}>My Reviews</NavLink></div> */}
+                        {/* <div><LogoutButton /></div> */}
+                    </div>
+                </div>
             </div>
+            // <div className='nav-bar1-account-dropdown'>
+            //     <div>
+            //         <div><NavLink to={`/login`}><button>Sign In</button></NavLink></div>
+            //         <div>
+            //             New customer?
+            //             <NavLink to={`/sign-up`}> Start here.</NavLink>
+            //         </div>
+            //     </div>
+            //     <div>
+            //         <div>Your Selling Account</div>
+            //         <div><NavLink to={`/login`}>Products</NavLink></div>
+            //     </div>
+            //     <div>
+            //         <div>Your Account</div>
+            //         <div><NavLink to={`/login`}>Account</NavLink></div>
+            //         <div><NavLink to={`/login`}>Orders</NavLink></div>
+            //         {/* <div><NavLink to={`/login`}>My Favorites</NavLink></div> */}
+            //         {/* <div><NavLink to={`/login`}>My Reviews</NavLink></div> */}
+            //         <div><LogoutButton /></div>
+            //     </div>
+            // </div>
         );
     }
 
@@ -52,22 +94,21 @@ function AccountList() {
     }, [user]);
 
 
-
     return (
         <div className='nav-bar1-account-container'>
-            {user && <div>Hello, {name}</div>}
-            {!user && <div>Hello, sign in</div>}
-            <div className='nav-bar1-account-2'>
-                <div>Account & Lists</div>
-                <img src={arrow} className='nav-bar1-account-2-arrow' />
+            <div className='nav-bar1-account-sec'>
+                {user && <div className='nav-bar1-account-1'>Hello, {name}</div>}
+                {!user && <div className='nav-bar1-account-1'>Hello, sign in</div>}
+                <div className='nav-bar1-account-2'>
+                    <div>Account & Lists</div>
+                    <img src={arrow} className='nav-bar1-account-2-arrow' />
+                </div>
             </div>
             <div>
                 {sessionLinks}
             </div>
         </div>
     )
-
-
 };
 
 export default AccountList;
