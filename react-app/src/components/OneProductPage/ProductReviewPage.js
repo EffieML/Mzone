bc295c725546841bd874b7fcf4c649387b7cef89
mingliu <ming-liu@outlook.com>
2022-12-25 19:59:40 -0600

    one product info section

--- react-app/src/components/OneProductPage/ProductReviewPage.js
+++ react-app/src/components/OneProductPage/ProductReviewPage.js
@@ -1,6 +1,7 @@
 import { useParams, NavLink, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux'
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import StarRatings from 'react-star-ratings';
 import { getProductReviewsThunk, deleteReviewThunk } from '../../store/review';
 import EditReviewModal from '../EditReviewModal';
 import './ProductReviewPage.css';
@@ -38,52 +39,63 @@ function ProductReview({ productId }) {
     }
 
     return (
-        <div>
-            <hr></hr>
-            <div>product review page</div>
-            <div>
-                <hr></hr>
-                <div>Review this product</div>
-                <div>Share your thoughts with other customers</div>
-                <NavLink to={`/products/${productId}/addreview`}>
-                    <button>Write a customer review</button>
-                </NavLink>
-                <hr></hr>
+        <div className='one-prod-bttm-container'>
+            <div className='one-prod-bttm-left-container'>
+                <div className='one-prod-bttm-left1'>
+                    <h2 id='title'>Customer reviews</h2>
+                    <div className='one-prod-middle-td-star1'>
+                        <StarRatings
+                            rating={avgRating(reviews)}
+                            starRatedColor='rgb(242, 201, 0)'
+                            starEmptyColor='rgb(206, 212, 212)'
+                            starDimension='16px'
+                            starSpacing='0px'
+                            numberOfStars={5}
+                            name='rating'
+                            className="one-prod-top-middle-review-star"
+                        />
+                    </div>
+                    <div>{avgRating(reviews)} out of 5</div>
+                    <div>{reviews.length} global ratings</div>
+                </div>
+                <div className='one-prod-bttm-left2'>
+                    <hr></hr>
+                    <div>Review this product</div>
+                    <div>Share your thoughts with other customers</div>
+                    <NavLink to={`/products/${productId}/addreview`}>
+                        <button>Write a customer review</button>
+                    </NavLink>
+                    <hr></hr>
+                </div>
+
             </div>
             {reviews.length && (
-                <div>
+                <div className='one-prod-bttm-right-container'>
+                    <div>Top reviews from the United States</div>
                     <div>
-                        <div>Customer reviews</div>
-                        <div>{avgRating(reviews)} out of 5</div>
-                        <div>{reviews.length} global ratings</div>
+                        {reviews?.map(reviewE => (
+                            <div key={reviewE.id}>
+                                <hr></hr>
+                                <div>user logo</div>
+                                <div>{reviewE?.user.firstname} {reviewE?.user.lastname}</div>
+                                {user?.id == reviewE?.user.id && (
+                                    <div>
+                                        <div>
+                                            <EditReviewModal reviewE={reviewE} reviewId={reviewE.id} />
+                                        </div>
+                                        <button onClick={() => handleReviewDelete(reviewE.id)}> delete review</button>
+                                    </div>
+                                )}
+                                {/* <button onClick={() => handleDelete(spot.id)}> Delete Listing </button> */}
+                                <div>{reviewE.stars}</div>
+                                <div>{reviewE.title}</div>
+                                <div>Reviewed in the United States on {convertDate(reviewE.createdAt)}</div>
+                                <div>??? Verified Purchase</div>
+                                <div>{reviewE.review}</div>
+                            </div>
+                        ))}
                     </div>
 
-                    <div>
-                        <div>Top reviews from the United States</div>
-                        <div>
-                            {reviews?.map(reviewE => (
-                                <div key={reviewE.id}>
-                                    <hr></hr>
-                                    <div>user logo</div>
-                                    <div>{reviewE?.user.firstname} {reviewE?.user.lastname}</div>
-                                    {user?.id == reviewE?.user.id && (
-                                        <div>
-                                            <div>
-                                                <EditReviewModal reviewE={reviewE} reviewId={reviewE.id} />
-                                            </div>
-                                            <button onClick={() => handleReviewDelete(reviewE.id)}> delete review</button>
-                                        </div>
-                                    )}
-                                    {/* <button onClick={() => handleDelete(spot.id)}> Delete Listing </button> */}
-                                    <div>{reviewE.stars}</div>
-                                    <div>{reviewE.title}</div>
-                                    <div>Reviewed in the United States on {convertDate(reviewE.createdAt)}</div>
-                                    <div>??? Verified Purchase</div>
-                                    <div>{reviewE.review}</div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
                 </div>
             )}
 
