aeb282710ff69d7126b072aefaa23a871eef2a82
mingliu <ming-liu@outlook.com>
2022-12-27 16:14:42 -0600

    signup page css

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
-import { Redirect } from 'react-router-dom';
+import { Redirect, NavLink } from 'react-router-dom';
 import { signUp } from '../../store/session';
+import mzonelogo from '../../img/mzonelogowhite.png'
+import './LoginForm.css';
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
@@ -53,69 +55,99 @@ const SignUpForm = () => {
   }
 
   return (
-    <form onSubmit={onSignUp}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <div className='login-page-container'>
+      <div className='login-page-container-inner'>
+        <NavLink to={`/`}>
+          <img src={mzonelogo} className='login-page-logo' />
+        </NavLink>
+        <div className='login-page-form-container'>
+          <form onSubmit={onSignUp}>
+            <div className='login-page-form-title'>Create account</div>
+            <div>
+              {errors.map((error, ind) => (
+                <div className='login-page-form-errors-container'>
+                  <div className='login-page-form-errors1'>!</div>
+                  <div key={ind}>{error}</div>
+                </div>
+              ))}
+            </div>
+            <div className='login-page-form-email-container'>
+              <label className='login-page-form-email1'>First Name</label>
+              <input
+                type='text'
+                name='firstname'
+                onChange={updateFirstname}
+                value={firstname}
+              ></input>
+            </div>
+            <div className='login-page-form-email-container'>
+              <label className='login-page-form-email1'>Last Name</label>
+              <input
+                type='text'
+                name='lastname'
+                onChange={updateLastname}
+                value={lastname}
+              ></input>
+            </div>
+            <div className='login-page-form-email-container'>
+              <label className='login-page-form-email1'>User Name</label>
+              <input
+                type='text'
+                name='username'
+                onChange={updateUsername}
+                value={username}
+              ></input>
+            </div>
+            <div className='login-page-form-email-container'>
+              <label className='login-page-form-email1'>Email</label>
+              <input
+                type='text'
+                name='email'
+                onChange={updateEmail}
+                value={email}
+              ></input>
+            </div>
+            <div className='login-page-form-email-container'>
+              <label className='login-page-form-email1'>Password</label>
+              <input
+                type='password'
+                name='password'
+                placeholder='At least 6 characters'
+                onChange={updatePassword}
+                value={password}
+              ></input>
+              <div className='signup-page-form-password3'>
+                <div className='signup-page-form-password3icon'>i</div>
+                <div className='signup-page-form-password3word'> Passwords must be at least 6 characters.</div>
+              </div>
+            </div>
+            <div className='login-page-form-email-container'>
+              <label className='login-page-form-email1'>Re-enter password</label>
+              <input
+                type='password'
+                name='repeat_password'
+                onChange={updateRepeatPassword}
+                value={repeatPassword}
+              // required={true}
+              ></input>
+            </div>
+            <button type='submit'>Sign Up</button>
+            <div className='login-page-form-note'>
+              By continuing, you agree to Mzone's Conditions of Use and Privacy Notice.
+            </div>
+          </form>
+          <div className='signup-page-line1'></div>
+          <div className='signup-page-loginacct'>
+            <div className='signup-page-haveacct'>
+              Already have an account?
+            </div>
+            <NavLink to={`/login`}>Sign in</NavLink>
+          </div>
+
+
+        </div>
       </div>
-      <div>
-        <label>First Name</label>
-        <input
-          type='text'
-          name='firstname'
-          onChange={updateFirstname}
-          value={firstname}
-        ></input>
-      </div>
-      <div>
-        <label>Last Name</label>
-        <input
-          type='text'
-          name='lastname'
-          onChange={updateLastname}
-          value={lastname}
-        ></input>
-      </div>
-      <div>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
-    </form>
+    </div>
   );
 };
 
