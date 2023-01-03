2b0ea8cef778287099e1dd48af60dd275afe4d3f
mingliu <ming-liu@outlook.com>
2023-01-03 17:20:58 -0600

    404 page

---
+++ react-app/src/components/404Page/index.js
@@ -0,0 +1,17 @@
+import { Redirect, useHistory } from "react-router-dom";
+import React from "react";
+import "./PageNotFound.css"
+
+function FourOhFourPage() {
+    const history = useHistory()
+    return (
+        <div className="pageNotFound">
+            {/* <h1>Page not found</h1> */}
+            <h2>404 Page, Redirecting</h2>
+            <Redirect to={"/"} />
+        </div>
+    )
+}
+
+
+export default FourOhFourPage
