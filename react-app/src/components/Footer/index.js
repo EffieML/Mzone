ca3efeb033691c96a91bf0f436daa7d35b55e90d
mingliu <ming-liu@outlook.com>
2023-01-18 10:47:14 -0600

    footer

---
+++ react-app/src/components/Footer/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom'
+import mzonelogo from '../../img/mzonelogo.png'
+import githublogo from '../../img/githublogo.png'
+import linkedin from '../../img/linkedin.jpg'
+import portfolio from '../../img/portfolio.png'
+import './Footer.css';
+
+function Footer() {
+
+    return (
+        <div className='footer-container'>
+            <div className='footer-container-inner'>
+                <div className='footer-logo'>
+
+                    <img src={mzonelogo} className='nav-bar1-logo-img' alt='mzone' />
+
+                </div>
+                <div className='footer-links'>
+                    <div className='footer-logoname1'>Ming Liu</div>
+                </div>
+                <div className='footer-logo-container'>
+                    <a href='https://effieml.github.io/'>
+                        <div className='footer-links'>
+                            <img src={portfolio} alt='linkedin' />
+                            <div className='footer-logoname'>Portfolio</div>
+                        </div>
+                    </a>
+                </div>
+                <div className='footer-logo-container'>
+                    <a href='https://www.linkedin.com/in/ming-liu-86611695/'>
+                        <div className='footer-links1'>
+                            <img src={linkedin} alt='linkedin' />
+                            <div className='footer-logoname'>LinkedIn</div>
+                        </div>
+                    </a>
+                </div>
+                <div className='footer-logo-container'>
+                    <a href='https://github.com/EffieML/Mzone'>
+                        <div className='footer-links'>
+                            <img src={githublogo} alt='github' />
+                            <div className='footer-logoname'>GitHub</div>
+                        </div>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    )
+
+
+}
+
+export default Footer;
